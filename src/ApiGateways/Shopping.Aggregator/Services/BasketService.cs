71773e6a350b0d2197ae8240678ec74d357180b0
Islam Elsherbini <hte.first.fiddler167@gmail.com>
2021-07-24 22:42:51 +0300

    Complete Shopping.Aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,20 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient) =>
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        public async Task<BasketModel> GetBasket(string userName) =>
+            await ( await _httpClient.GetAsync($"/api/v1/Basket/{userName}")).ReadContentAs<BasketModel>();
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/CaltalogService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CaltalogService : ICatalogService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CaltalogService(HttpClient httpClient) =>
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        public async Task<IEnumerable<CatalogModel>> GetCatalog() =>
+            await (await _httpClient.GetAsync("/api/v1/Catalog")).ReadContentAs<List<CatalogModel>>();
+        public async Task<CatalogModel> GetCatalog(string id) =>
+            await (await _httpClient.GetAsync($"/api/v1/Catalog/{id}")).ReadContentAs<CatalogModel>();
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category) =>
+             await (await _httpClient.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}")).ReadContentAs<IEnumerable<CatalogModel>>();
+    }
+}
